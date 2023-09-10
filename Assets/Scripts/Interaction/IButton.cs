185830e55301e06c7f6bd601727d8184555d5fd9
broanite <lmcint218@caledonian.ac.uk>
2023-09-10 17:21:41 +0100

    Button can activate by walking into it

--- Assets/Scripts/Interaction/IButton.cs
+++ Assets/Scripts/Interaction/IButton.cs
@@ -1,39 +1,68 @@
+using System;
 using System.Collections;
 using UnityEngine;
 public class IButton : AddDelegate
 {
     [Header("Interactable")]
 
+    [SerializeField] float deactivationTime = 7.5f;
+
     bool active = false;
 
+    float startScale;
+    private void Awake()
+    {
+        startScale = transform.localScale.z;
+    }
     // Collision Effects
     private void OnTriggerEnter(Collider other)
     {
         if (active)
             return;
         ButtonEffect(other);
-
-        CallDelegate();
     }
 
     void ButtonEffect(Collider hit)
     {
-        if (hit.name == "Bullet(Clone)")
+        if (hit.name == "Bullet(Clone)" || hit.gameObject.layer == LayerMask.NameToLayer("Player"));
         {
             active = true;
             StartCoroutine(ActivateButton());
         }
     }
+
     IEnumerator ActivateButton()
     {
-        Debug.Log("Button Activated");
-        float startScale = transform.localScale.z;
+        float currentScale = transform.localScale.z;
+        float finalScale = startScale * .25f;
+
         float totalTime = .5f;
         for (float time = 0; time < totalTime; time += Time.deltaTime)
         {
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Lerp(startScale, startScale * .25f, time / totalTime));
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Lerp(currentScale, finalScale, time / totalTime));
             yield return new WaitForFixedUpdate();
         }
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, startScale * .25f);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, finalScale);
+
+        CallDelegate();
+
+        StartCoroutine(DeActivateButton(deactivationTime));
+    }
+
+    IEnumerator DeActivateButton(float delayTime)
+    {
+        float currentScale = transform.localScale.z;
+        float finalScale = startScale;
+
+        float totalTime = delayTime;
+        for (float time = 0; time < totalTime; time += Time.deltaTime)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Lerp(currentScale, finalScale, time / totalTime));
+            yield return new WaitForFixedUpdate();
+        }
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, finalScale);
+
+        CallDelegate();
+        active = false;
     }
 }
